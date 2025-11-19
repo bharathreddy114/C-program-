4b758e9a3eb4499a78b5e8a8d688de6b163bb2e7
bharathreddy114 <nrbharathreddy06@gmail.com>
2025-11-19 09:47:11 +0530

    Update P9.c

--- P9.c
+++ P9.c
@@ -4,18 +4,15 @@ int main() {
     int marks[5], total = 0;
     float percentage;
     char grade;
-    
-    // Input marks for 5 subjects
     printf("Enter marks for 5 subjects: ");
     for (int i = 0; i < 5; i++) {
         scanf("%d", &marks[i]);
         total += marks[i];
     }
     
-    // Calculate percentage
     percentage = (float)total / 5;
     
-    // Determine grade based on percentage
+    
     if (percentage >= 90) {
         grade = 'A';
     } else if (percentage >= 80) {
